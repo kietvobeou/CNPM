e61ec0a96dc863249a60e24dc7a6b73cba8abeee
kietvobeou <kietvo2999@gmail.com>
2025-12-08 22:24:49 +0700

    Lam trang Home

---
+++ CNPM/Content/Home.js
@@ -0,0 +1,20 @@
+﻿// Tự động chạy carousel mỗi 5 giây
+document.addEventListener('DOMContentLoaded', function () {
+    var promotionCarousel = document.getElementById('promotionCarousel');
+    if (promotionCarousel) {
+        var carousel = new bootstrap.Carousel(promotionCarousel, {
+            interval: 5000, // 5 giây
+            ride: 'carousel',
+            wrap: true
+        });
+    }
+
+    // Thêm hiệu ứng click cho các promotion images
+    var promotionLinks = document.querySelectorAll('.promotion-link');
+    promotionLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            // Có thể thêm hiệu ứng loading hoặc tracking ở đây
+            console.log('Đang chuyển hướng đến chi tiết khuyến mãi');
+        });
+    });
+});
---
+++ CNPM/Controllers/CinemaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNPM.Controllers
+{
+    public class CinemaController : Controller
+    {
+        QuanLyRapPhimEntities db = new QuanLyRapPhimEntities();
+        public ActionResult Index()
+        {
+            ViewBag.PhimSC = db.PHIMs
+             .Where(p => p.TrangThai == "Sắp chiếu")
+             .OrderBy(r => Guid.NewGuid())
+             .Take(4)
+             .ToList();
+
+            ViewBag.PhimDC = db.PHIMs
+                .Where(p => p.TrangThai == "Đang chiếu")
+                .OrderBy(r => Guid.NewGuid())
+                .Take(4)
+                .ToList();
+
+            ViewBag.Tin = db.TIN_TUC
+                .OrderBy(r => Guid.NewGuid())
+                .Take(4)
+                .ToList();
+            return View();
+        }
+    }
+}
