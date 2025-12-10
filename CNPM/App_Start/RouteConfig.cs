e6b229f181812538a76e592fb286086851e498b8
kietvobeou <kietvo2999@gmail.com>
2025-12-10 18:22:58 +0700

    Hoan thien chi tiet, dat ve va thanh toan

--- CNPM/App_Start/RouteConfig.cs
+++ CNPM/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CNPM
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Cinema", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
