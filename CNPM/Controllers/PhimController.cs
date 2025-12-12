4fca68c4d309e9ae789ad13a023f81a3e8dcae8b
Lê Viết Tuấn Hưng <tomhung911@gmail.com>
2025-12-12 23:09:05 +0700

    Them trang Delete phim + Trang Edit

--- CNPM/Controllers/PhimController.cs
+++ CNPM/Controllers/PhimController.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using CNPM;   
+using System.IO;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using CNPM;   
 
 namespace CNPM.Controllers
 {
@@ -21,5 +23,77 @@ namespace CNPM.Controllers
             var list = db.PHIM.Where(x => x.TrangThai == "Sắp chiếu").ToList();
             return View(list);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PHIM model, HttpPostedFileBase AnhBiaFile)
+        {
+            if (ModelState.IsValid)
+            {
+                // 1. Thêm phim vào DB trước để có IDPhim
+                db.PHIM.Add(model);
+                db.SaveChanges();
+
+                // 2. Nếu có upload ảnh
+                if (AnhBiaFile != null && AnhBiaFile.ContentLength > 0)
+                {
+                    string extension = Path.GetExtension(AnhBiaFile.FileName);
+                    string fileName = "AnhBia_" + model.IDPhim + extension;
+                    string path = Path.Combine(Server.MapPath("~/Content/HinhAnh"), fileName);
+
+                    AnhBiaFile.SaveAs(path);
+
+                    // 3. Cập nhật lại tên file ảnh vào DB
+                    model.AnhBia = fileName;
+                    db.SaveChanges();
+                }
+
+                // Trở về trang Sắp chiếu (hoặc Đang chiếu)
+                return RedirectToAction("SapChieu");
+            }
+
+            return View(model);
+        }
+        public ActionResult Delete(int id)
+        {
+            var phim = db.PHIM.Find(id);
+
+            if (phim == null)
+                return HttpNotFound();
+
+            return View(phim);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var phim = db.PHIM.Find(id);
+
+            if (phim == null)
+                return HttpNotFound();
+
+            // ========================
+            // XÓA ẢNH BÌA NẾU TỒN TẠI
+            // ========================
+            if (!string.IsNullOrEmpty(phim.AnhBia))
+            {
+                string path = Server.MapPath("~/Content/HinhAnh/" + phim.AnhBia);
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+            }
+
+            // Xóa phim trong database
+            db.PHIM.Remove(phim);
+            db.SaveChanges();
+
+            return RedirectToAction("SapChieu"); // Hoặc DangChieu tùy bạn
+        }
+
     }
 }
